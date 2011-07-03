cd2d11171d0e9bace958f852437d988340703edb
Seth Fuller <fuller.sethm@gmail.com>
2011-07-03 14:55:38 -0400

    Made the @fibonacci_sequence a class instance variable

--- lib/fibonacci_calc.rb
+++ lib/fibonacci_calc.rb
@@ -10,21 +10,25 @@ class Fixnum
 
   private
 
-  @@fibonacci_sequence = [0, 1]
+  @fibonacci_sequence = [0, 1]
+
+  class << self
+    attr_accessor :fibonacci_sequence
+  end
 
   def find_fibonacci(num)
     best_fibonacci = -1
     best_fibonacci = 0 if num == 1
     last_index = -1
     while best_fibonacci < 0
-      last_fibonacci = @@fibonacci_sequence[last_index]
-      prev_fibonacci = @@fibonacci_sequence[last_index - 1]
+      last_fibonacci = Fixnum.fibonacci_sequence[last_index]
+      prev_fibonacci = Fixnum.fibonacci_sequence[last_index - 1]
       if last_fibonacci >= num && prev_fibonacci < num
         best_fibonacci = prev_fibonacci
       elsif prev_fibonacci >= num
         last_index -= 1
       else
-        @@fibonacci_sequence << last_fibonacci + prev_fibonacci
+        Fixnum.fibonacci_sequence << last_fibonacci + prev_fibonacci
       end
     end
 
